bfb64fe0054518453a8e1c5540c05b1a4750fa0f
Pranav Sanvatsarkar <pranav.sanvatsarkar@enzigma.com>
2015-02-26 15:26:19 +0530

    - after adding index.html

---
+++ application_js/assignment-app.js
@@ -0,0 +1,80 @@
+﻿angular.module('assignment-app', ['enzi-table', 'enzi-form'])
+.factory('$enziForm', ['$rootScope', '$compile', function ($rootScope, $compile) {
+    var $enziForm = {
+        loadForm: function (fields, record, columns, records) {
+            $rootScope.fields = fields;
+            $rootScope.record = record;
+            $rootScope.records = records;
+            $rootScope.columns = columns;
+            var dialog;
+            var $enziForm = {
+                updateRecords: function (record) {
+                    $rootScope.records.push(record);
+                    dialog.remove();
+                }
+            }
+            $rootScope.$enziForm = $enziForm;
+            if (record)
+                dialog = angular.element('<div enzi-form="" record="record" fields="fields" columns="columns" action="Save" title="Update record" callback="$enziForm.updateRecords(record)">');
+            else {
+                $rootScope.record = {};
+                dialog = angular.element('<div enzi-form="" record="record" fields="fields" columns="columns" action="Create" title="Create record" callback="$enziForm.updateRecords(record)">');
+            }
+            dialog = $compile(dialog)($rootScope);
+            $('body').append(dialog);
+        }
+    };
+    return $enziForm;
+}])
+.controller('appController', ['$scope', '$enziForm', function ($scope, $enziForm) {
+    $scope.fields = [
+	    { name: 'Id', label: 'Id', type: 'number', required: true },
+        { name: 'EmployeeId', label: 'Employee Id', type: 'text', required: true },
+	    { name: 'FirstName', label: 'First Name', type: 'text', required: true },
+        { name: 'LastName', label: 'Last Name', type: 'text', required: true },
+	    { name: 'Email', label: 'Email', type: 'email', required: false },
+	    { name: 'MailingStreet', label: 'Street', type: 'textarea' },
+        { name: 'MailingCity', label: 'City', type: 'text' },
+        { name: 'Phone', label: 'Phone', type: 'phone' },
+	    { name: 'DOB', label: 'Date of Birth', type: 'date', required: false },
+	    { name: 'MailingCountry', label: 'Country', type: 'picklist', required: false, picklistValues: ['India', 'United States'] },
+	    { name: 'MailingState', label: 'State', type: 'picklist', required: false, picklistValues: { "India": ['Gujarat', 'Maharashtra'], 'United States': ['California', 'New York'] }, dependsOn: 'MailingCountry' },
+    ];
+
+    $scope.columns = [
+        { name: 'Id', label: 'Id', template: "{{record.Id}}" },
+        { name: 'EmployeeId', label: 'Employee Id', template: "{{record.EmployeeId}}", editFields: 'EmployeeId' },
+	    { name: 'FirstName', label: 'First Name', template: "{{record.FirstName}}", editFields: 'FirstName' },
+	    { name: 'LastName', label: 'Last Name', template: "{{record.LastName}}", editFields: 'LastName' },
+	    { name: 'Email', label: 'Email', template: "{{record.Email}}", editFields: 'Email' },
+        { name: 'Address', label: 'Address', template: "{{record.MailingStreet + ',' + record.MailingCountry}}", editFields: 'MailingCountry, MailingState, MailingCity, MailingStreet' },
+	    { name: 'Phone', label: 'Phone', template: "{{record.Phone}}", editFields: 'Phone' },
+	    { name: 'DOB', label: 'Date of Birth', template: "{{record.DOB | date : shortDate}}", editFields: 'DOB' }
+    ];
+
+    $scope.records = [
+        { Id: 1, EmployeeId: '123', FirstName: 'User', LastName: '1', Email: 'user1@test.com', MailingStreet: 'Street1', MailingCity: 'City1', DOB: new Date(), MailingCountry: 'India', MailingState: 'Maharashtra' },
+        { Id: 2, EmployeeId: '234', FirstName: 'User', LastName: '2', Email: 'user2@test.com', MailingStreet: 'Street1', MailingCity: 'City1', DOB: new Date(), MailingCountry: 'India', MailingState: 'Maharashtra' },
+        { Id: 3, EmployeeId: '456', FirstName: 'User', LastName: '3', Email: 'user3@test.com', MailingStreet: 'Street1', MailingCity: 'City1', DOB: new Date(), MailingCountry: 'India', MailingState: 'Maharashtra' }];
+
+    $scope.DeleteRecord = function (record, onsuccess) {
+        $scope.records.splice($scope.records.indexOf(record), 1);
+        //Use code to delete record from Salesforce and on success call the onsuccess function
+        //onsuccess();
+    }
+
+    $scope.EditRecord = function (record, onsuccess) {
+        $enziForm.loadForm($scope.fields, record, $scope.columns, $scope.records);
+        //var recordUpdated = angular.copy(record);
+        //recordUpdated.LastName += 2;
+        //onsuccess(recordUpdated);
+    }
+
+    $scope.CreateRecord = function () {
+        $enziForm.loadForm($scope.fields, undefined, $scope.columns, $scope.records);
+    }
+
+    $scope.update = function (record) {
+        $scope.records.push(record);
+    }
+}])
---
+++ application_js/enzi-table.js
@@ -0,0 +1,137 @@
+﻿angular.module('enzi-table', [])
+.directive('enziField', function () {
+    return {
+        controller: ['$scope', '$element', '$compile', '$attrs', function ($scope, $element, $compile, $attrs) {
+            var fieldName = $attrs.enziField;
+            var field = $scope.fieldsmap[fieldName];
+            var input = '<form name="inlineEdit">';
+            switch (field.type) {
+                case 'textarea': input += '<textarea ng-blur="updateField(\'' + fieldName + '\')" type="' + field.type + '" ng-model="record[\'' + fieldName + '\']" class="form-control"></textarea>'; break;
+                default: input += '<input ng-blur="updateField(\'' + fieldName + '\')" type="' + field.type + '" ng-model="record[\'' + fieldName + '\']" class="form-control"/>'; break;
+            }
+            input += '</form>';
+            input = angular.element(input);
+            $element.append($compile(input)($scope));
+            setTimeout(function () { input.focus(); }, 100);
+        }]
+    }
+})
+.directive('enziRow', function () {
+    return {
+        scope: {
+            record: '=enziRow',
+            fields: '=',
+            columns: '=',
+            fieldsmap: '=',
+            columnsmap: '=',
+        },
+        controller: ['$scope', '$element', '$compile', function ($scope, $element, $compile) {
+            var row = '';
+            $scope.editor = {};
+            angular.forEach($scope.columns, function (column) {
+                row += '<td id="' + $scope.record.Id + '_' + column.name + '" ng-dblclick="onedit(\'' + column.name + '\')"><span ng-hide="editor.' + column.name + '">' + column.template + '</span></td>';
+            })
+            $element.append($compile(angular.element(row))($scope));
+
+            $scope.onedit = function (columnName) {
+                if (!$scope.editor[columnName]) {
+                    var column = $scope.columnsmap[columnName];
+                    if (column.editFields) {
+                        $scope.editor[columnName] = true;
+                        var editFields = column.editFields.split(',');
+                        if (editFields.length == 1) {
+                            $element.find('#' + $scope.record.Id + '_' + columnName).append($compile('<span enzi-field="' + editFields[0] + '"></span>')($scope));
+                        }
+                        else {
+                            $scope.oldRecord = {};
+                            angular.forEach(editFields, function (field) {
+                                $scope.oldRecord[field.trim()] = $scope.record[field.trim()];
+                            });
+                            var inputs = '<div class="modal fade in" style="display:block;" id="addressForm">\
+                                <div class="modal-dialog">\
+                                <div class="modal-content">\
+                                    <div class="modal-header">\
+                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true" ng-click="closeDialog(\'' + columnName + '\',\'addressForm\')">×</button>\
+                                    <h4 class="modal-title">Address</h4>\
+                                    </div>\
+                                    <div class="modal-body">';
+                            angular.forEach(editFields, function (fieldName) {
+                                inputs += '<span enzi-field="' + fieldName + '"><label>'+fieldName+'</label></span>';
+                            })
+                            inputs += '   </div>\
+                                    <div class="modal-footer">\
+                            <button class="btn btn-primary" ng-click="saveRecord(\'' + columnName + '\',\'addressForm\')">Save</button></div>\
+                            </div>\
+                            </div>\
+                            </div>';
+                            $element.find('#' + $scope.record.Id + '_' + columnName).append($compile(inputs)($scope));
+                        }
+                    }
+                        
+                }
+            }
+            $scope.saveRecord = function (columnName, elementId) {
+                $('#' + elementId).remove();
+                $scope.editor[columnName] = false;
+            }
+            $scope.closeDialog = function (columnName, elementId) {
+                var column = $scope.columnsmap[columnName];
+                if (column.editFields) {
+                    angular.forEach(column.editFields.split(','), function (field) {
+                        $scope.record[field.trim()] = $scope.oldRecord[field.trim()];
+                    });
+                }
+                $('#' + elementId).remove();
+                $scope.editor[columnName] = false;
+            }
+
+            $scope.updateField = function (columnName) {
+                var input = $element.find('#' + $scope.record.Id + '_' + columnName).find('[enzi-field]');
+                if (!$scope.inlineEdit.$invalid) {
+                    $scope.editor[columnName] = false;
+                    input.remove();
+                }
+            }
+        }]
+    }
+})
+.directive('enziTable', function () {
+    return {
+        templateUrl: application_js + 'templates/enzi-table.htm',
+        scope: {
+            records: '=',
+            fields: '=',
+            columns: '=',
+            ondelete: '&',
+            onedit: '&',
+        },
+        controller: ['$scope', '$element', '$compile', function ($scope, $element, $compile) {
+            $scope.fieldsMap = {};
+            angular.forEach($scope.fields, function (field) { $scope.fieldsMap[field.name] = field; });
+
+            $scope.columnsMap = {};
+            angular.forEach($scope.columns, function (column) { $scope.columnsMap[column.name] = column; });
+
+            $scope.editRecord = function (record) {
+                $scope.onedit({
+                    record: record, onsuccess: function (recordUpdated) {
+                        var found = $scope.records.indexOf(record);
+                        if (found >= 0)
+                            $scope.records.splice(found, 1, recordUpdated);
+                    }
+                });
+            };
+            $scope.deleteRecord = function (record) {
+                if (confirm('Are you sure you want to delete the record?')) {
+                    $scope.ondelete({
+                        record: record, onsuccess: function () {
+                            var found = $scope.records.indexOf(record);
+                            if (found >= 0)
+                                $scope.records.splice(found, 1);
+                        }
+                    });
+                }
+            };
+        }]
+    }
+})
